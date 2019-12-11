2f8d8128300ec776ab638e3a149d967bff5fbfe2
Ben Mandakh <benmandakh@gmail.com>
2019-12-11 01:57:08 -0800

    Last edit by Ben

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+// Developed by Ben Mandakh, 01:22am 12/11/2019
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
